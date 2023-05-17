f54d62f3c7e39b2638f2068e8ff07118f8e52cc5
vspaz <vladimir.edelman@gmail.com>
2023-05-17 20:38:06 +0200

    add files.CreateFile

--- pkg/files/files.go
+++ pkg/files/files.go
@@ -15,7 +15,15 @@ func FromFile(path string) *os.File {
 	log.Printf("trying to read file at '%s'\n", path)
 	fh, err := os.Open(path)
 	if err != nil {
-		log.Printf("error reading a file: '%s'\n", err)
+		log.Fatalf("error reading a file: '%s'\n", err)
+	}
+	return fh
+}
+
+func CreateFile(path string) *os.File {
+	fh, err := os.Create(path)
+	if err != nil {
+		log.Fatalf("failed to create a file: '%s'\n", err)
 	}
 	return fh
 }
