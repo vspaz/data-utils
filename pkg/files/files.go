019633bd23fdb99ffa3587bf284aaef27ca8895d
vspaz <vladimir.edelman@gmail.com>
2023-05-17 20:34:25 +0200

    add files.FromFile

--- pkg/files/files.go
+++ pkg/files/files.go
@@ -1,7 +1,21 @@
 package files
 
+import (
+	"log"
+	"os"
+)
+
 func MustClose(fh *os.File) {
 	if err := fh.Close(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func FromFile(path string) *os.File {
+	log.Printf("trying to read file at '%s'\n", path)
+	fh, err := os.Open(path)
+	if err != nil {
+		log.Printf("error reading a file: '%s'\n", err)
+	}
+	return fh
 }
