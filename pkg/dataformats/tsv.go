54da0c7db05fa07fcbd5fa5334bc4e1ef34cc776
vspaz <vladimir.edelman@gmail.com>
2023-05-18 11:02:14 +0200

    add RowIterator

--- pkg/dataformats/tsv.go
+++ pkg/dataformats/tsv.go
@@ -12,3 +12,23 @@ type RowIterator struct {
 	fileFormat string // csv | tsv
 	currentRow []string
 }
+
+func (r RowIterator) Next() {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r RowIterator) HasNext() {
+	//TODO implement me
+	panic("implement me")
+}
+
+func NewRowIterator(reader *csv.Reader, fileFormat string) Iterator {
+	if fileFormat == "tsv" {
+		reader.Comma = '\t'
+	}
+	return &RowIterator{
+		csvReader:  reader,
+		fileFormat: fileFormat,
+	}
+}
