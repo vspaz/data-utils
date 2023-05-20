8465f0a886093778ffa0649ed6f4b7e5e8aa1f39
vspaz <vladimir.edelman@gmail.com>
2023-05-20 11:03:57 +0200

    extend the test

--- pkg/dataformats/tsv_test.go
+++ pkg/dataformats/tsv_test.go
@@ -8,12 +8,18 @@ import (
 )
 
 var tsvRecords = `
-foo1\tbar1\tbaz1
-foo2\tbar2\tbaz2
+foo1	bar1	baz1
+foo2	bar2	baz2
 `
 
 func TestNewRowIterator(t *testing.T) {
 	records := bufio.NewReader(strings.NewReader(tsvRecords))
 	iterator := NewRowIterator(records, "tsv")
+
 	assert.True(t, iterator.HasNext())
+	assert.Equal(t, "foo1", iterator.Next()[0])
+	assert.Equal(t, "bar1", iterator.Next()[1])
+	assert.Equal(t, "baz1", iterator.Next()[2])
+
+	assert.Equal(t, iterator.Next(), []string{"foo2", "bar2", "baz2"})
 }
