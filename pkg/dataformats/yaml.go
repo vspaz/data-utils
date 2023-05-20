a382a6d1db4c383c6cad4a5b5190c6ff216af346
vspaz <vladimir.edelman@gmail.com>
2023-05-21 00:06:33 +0200

    add 'NewYamlDecoder'

--- pkg/dataformats/yaml.go
+++ pkg/dataformats/yaml.go
@@ -1,1 +1,21 @@
 package dataformats
+
+import (
+	"gopkg.in/yaml.v2"
+	"io"
+	"log"
+)
+
+type YamlReader struct {
+	reader yaml.Decoder
+}
+
+func NewYamlDecoder(in io.Reader) *yaml.Decoder {
+	return yaml.NewDecoder(in)
+}
+
+func (y *YamlReader) FromYaml(body any) {
+	if err := y.reader.Decode(body); err != nil && err != io.EOF {
+		log.Fatalf("error decoding yaml %s", err)
+	}
+}
