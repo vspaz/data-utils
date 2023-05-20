7cece4bef61e603e2e90c0e9a0bf7cc6b7088c08
vspaz <vladimir.edelman@gmail.com>
2023-05-20 10:56:02 +0200

    add TestNewRotIterator

--- pkg/dataformats/tsv_test.go
+++ pkg/dataformats/tsv_test.go
@@ -1,1 +1,19 @@
 package dataformats
+
+import (
+	"bufio"
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+var tsvRecords = `
+foo1\tbar1\tbaz1
+foo2\tbar2\tbaz2
+`
+
+func TestNewRowIterator(t *testing.T) {
+	records := bufio.NewReader(strings.NewReader(tsvRecords))
+	iterator := NewRowIterator(records, "tsv")
+	assert.True(t, iterator.HasNext())
+}
