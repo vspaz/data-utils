ac2eeb6aa0199021dd2a9b9fc46320d802622db5
vspaz <vladimir.edelman@gmail.com>
2023-05-21 00:54:18 +0200

    add NewJsonDecoder

--- pkg/dataformats/json.go
+++ pkg/dataformats/json.go
@@ -2,7 +2,23 @@ package dataformats
 
 import (
 	"encoding/json"
+	"io"
+	"log"
 )
+
+type JsonDecoder struct {
+	decoder *json.Decoder
+}
+
+func NewJsonDecoder(in io.Reader) *JsonDecoder {
+	return &JsonDecoder{decoder: json.NewDecoder(in)}
+}
+
+func (j *JsonDecoder) FromJson(deserializable any) {
+	if err := j.decoder.Decode(deserializable); err != nil && err != io.EOF {
+		log.Fatalf("error decoding yaml %s", err)
+	}
+}
 
 func FromJson(deserializable []byte, deserialized any) error {
 	return json.Unmarshal(deserializable, deserialized)
