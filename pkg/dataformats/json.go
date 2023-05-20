d353be9d4a34ee8deec8ebb1912731a047950297
vspaz <vladimir.edelman@gmail.com>
2023-05-21 00:45:53 +0200

    add FromJson & ToJson

--- pkg/dataformats/json.go
+++ pkg/dataformats/json.go
@@ -1,1 +1,14 @@
 package dataformats
+
+import (
+	"encoding/json"
+)
+
+func FromJson(deserializable []byte, deserialized any) error {
+	return json.Unmarshal(deserializable, deserialized)
+}
+
+func ToJson(serializable any) []byte {
+	encodedMessage, _ := json.Marshal(serializable)
+	return encodedMessage
+}
