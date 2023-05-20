5d3830a934c3256afe56e97c162b1f9888ba5d5f
vspaz <vladimir.edelman@gmail.com>
2023-05-20 11:19:47 +0200

    add TestNewRowIteratorCsvOk

--- pkg/dataformats/tsv_test.go
+++ pkg/dataformats/tsv_test.go
@@ -7,19 +7,36 @@ import (
 	"testing"
 )
 
-var tsvRecords = `
+func TestNewRowIteratorTsvOk(t *testing.T) {
+	tsvRecords := `
 foo1	bar1	baz1
 foo2	bar2	baz2
 `
-
-func TestNewRowIterator(t *testing.T) {
 	records := bufio.NewReader(strings.NewReader(tsvRecords))
 	iterator := NewRowIterator(records, "tsv")
 
 	assert.True(t, iterator.HasNext())
-	assert.Equal(t, "foo1", iterator.Next()[0])
-	assert.Equal(t, "bar1", iterator.Next()[1])
-	assert.Equal(t, "baz1", iterator.Next()[2])
+	record1 := iterator.Next()
+	assert.Equal(t, []string{"foo1", "bar1", "baz1"}, record1)
 
-	assert.Equal(t, iterator.Next(), []string{"foo2", "bar2", "baz2"})
+	assert.True(t, iterator.HasNext())
+	record2 := iterator.Next()
+	assert.Equal(t, []string{"foo2", "bar2", "baz2"}, record2)
+}
+
+func TestNewRowIteratorCsvOk(t *testing.T) {
+	csvRecords := `
+foo1,bar1,baz1
+foo2,bar2,baz2
+`
+	records := bufio.NewReader(strings.NewReader(csvRecords))
+	iterator := NewRowIterator(records, "csv")
+
+	assert.True(t, iterator.HasNext())
+	record1 := iterator.Next()
+	assert.Equal(t, []string{"foo1", "bar1", "baz1"}, record1)
+
+	assert.True(t, iterator.HasNext())
+	record2 := iterator.Next()
+	assert.Equal(t, []string{"foo2", "bar2", "baz2"}, record2)
 }
