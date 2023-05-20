e64874bfa9e689fc1c885f9e8131744a280f964b
vspaz <vladimir.edelman@gmail.com>
2023-05-21 00:58:25 +0200

    add 'NewJsonEncoder'

--- pkg/dataformats/json.go
+++ pkg/dataformats/json.go
@@ -16,7 +16,22 @@ func NewJsonDecoder(in io.Reader) *JsonDecoder {
 
 func (j *JsonDecoder) FromJson(deserializable any) {
 	if err := j.decoder.Decode(deserializable); err != nil && err != io.EOF {
-		log.Fatalf("error decoding yaml %s", err)
+		log.Fatalf("error decoding json %s", err)
+	}
+}
+
+type JsonEncoder struct {
+	encoder *json.Encoder
+}
+
+func NewJsonEncoder(out io.Writer) *JsonEncoder {
+	return &JsonEncoder{encoder: json.NewEncoder(out)}
+}
+
+func (j *JsonEncoder) ToJson(serializable any) {
+	err := j.encoder.Encode(serializable)
+	if err != nil {
+		log.Fatalf("error encoding json %s", err)
 	}
 }
 
