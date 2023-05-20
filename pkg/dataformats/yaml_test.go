f85e707be8e4622533cac7d0296928ca5a05a877
vspaz <vladimir.edelman@gmail.com>
2023-05-21 00:35:28 +0200

    add TestNewYamlEncoderOk

--- pkg/dataformats/yaml_test.go
+++ pkg/dataformats/yaml_test.go
@@ -1,6 +1,7 @@
 package dataformats
 
 import (
+	"bytes"
 	"github.com/stretchr/testify/assert"
 	"strings"
 	"testing"
@@ -23,4 +24,19 @@ bar:
 	tempHolder := &TestObject{}
 	decoder.FromYaml(tempHolder)
 	assert.Equal(t, 3, tempHolder.Foo)
+}
+
+func TestNewYamlEncoderOk(t *testing.T) {
+	type TestObject struct {
+		Foo int      `yaml:"foo"`
+		Bar []string `yaml:"bar"`
+	}
+	tempHolder := &TestObject{
+		Foo: 10,
+		Bar: []string{"foo", "bar", "baz"},
+	}
+	out := new(bytes.Buffer)
+	encoder := NewYamlEncoder(out)
+	encoder.ToYaml(tempHolder)
+	assert.Equal(t, "foo: 10\nbar:\n- foo\n- bar\n- baz\n", out.String())
 }
