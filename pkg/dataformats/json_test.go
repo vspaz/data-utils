8cb4d7da611416e4e2ffbcae45a87045964224f6
vspaz <vladimir.edelman@gmail.com>
2023-05-22 21:20:00 +0200

    add TestNewJsonEncoderOk

--- pkg/dataformats/json_test.go
+++ pkg/dataformats/json_test.go
@@ -2,16 +2,24 @@ package dataformats
 
 import (
 	"bufio"
+	"bytes"
 	"github.com/stretchr/testify/assert"
 	"strings"
 	"testing"
 )
 
 func TestNewJsonDecoderOk(t *testing.T) {
-	jsonString := `{"foo":"bar"}`
+	jsonString := `{"foo":10}`
 	reader := bufio.NewReader(strings.NewReader(jsonString))
 	decoder := NewJsonDecoder(reader)
-	var keyToValue map[string]string
+	var keyToValue map[string]int
 	decoder.FromJson(&keyToValue)
-	assert.Equal(t, "bar", keyToValue["foo"])
+	assert.Equal(t, 10, keyToValue["foo"])
+}
+
+func TestNewJsonEncoderOk(t *testing.T) {
+	out := new(bytes.Buffer)
+	encoder := NewJsonEncoder(out)
+	encoder.ToJson(map[string]int{"foo": 10})
+	assert.Equal(t, "{\"foo\":10}\n", out.String())
 }
