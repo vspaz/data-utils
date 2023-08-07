6ecbe2ba6b7e41a692acb0ae9fdf33c23ff7bfac
vspaz <vladimir.edelman@gmail.com>
2023-08-07 23:01:40 +0200

    minor fixes & rename the package

--- pkg/filesystem/copytree.go
+++ pkg/filesystem/copytree.go
@@ -1,4 +1,4 @@
-package files
+package filesystem
 
 import (
 	"io"
@@ -9,16 +9,16 @@ import (
 // copytree provides similar functionality to Python shutil.copytree
 // https://docs.python.org/3.11/library/shutil.html#shutil.copytree
 
-// File implements file object interface.
-type File struct{}
+// FileSystem FileSystem implements a FileSystem interface.
+type FileSystem struct{}
 
-// New returns a File object.
-func New() *File {
-	return &File{}
+// New returns a FileSystem object.
+func New() *FileSystem {
+	return &FileSystem{}
 }
 
 // copyFile copes contents of the file named src to a file named dst.
-func (f *File) copyFile(src, dst string) error {
+func (fs *FileSystem) CopyFile(src, dst string) error {
 	in, err := os.Open(src)
 	if err != nil {
 		return err
@@ -40,7 +40,7 @@ func (f *File) copyFile(src, dst string) error {
 
 // CopyTree recursively creates and populates directory tree.
 // similar to python shutil.copytree
-func (f *File) CopyTree(src, dst string) error {
+func (fs *FileSystem) CopyTree(src, dst string) error {
 	files, err := os.ReadDir(src)
 	if err != nil {
 		return err
@@ -48,22 +48,21 @@ func (f *File) CopyTree(src, dst string) error {
 	if err = os.Mkdir(dst, os.ModePerm); err != nil {
 		return err
 	}
-	for _, fh := range files {
-		srcName := filepath.Join(src, fh.Name())
-		dstName := filepath.Join(dst, fh.Name())
+	for _, file := range files {
+		srcName := filepath.Join(src, file.Name())
+		dstName := filepath.Join(dst, file.Name())
 		fileInfo, err := os.Stat(srcName)
 		if err != nil {
 			return err
 		}
 
 		if fileInfo.IsDir() {
-			if err = f.CopyTree(srcName, dstName); err != nil {
+			if err = fs.CopyTree(srcName, dstName); err != nil {
 				return err
 			}
-		} else {
-			if err = f.copyFile(srcName, dstName); err != nil {
-				return err
-			}
+		}
+		if err = fs.CopyFile(srcName, dstName); err != nil {
+			return err
 		}
 	}
 	return nil
